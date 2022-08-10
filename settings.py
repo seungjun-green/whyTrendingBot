6adf9b3d88c4ddd66004fd87dfe06726959baeec
seungjun-green <lsj3285007@gmail.com>
2022-08-10 22:48:37 +0900

    testing

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 user_id = 'whyTrendingBot'
 production = False
+
 use_example = False
 
 example_data = [{'tweet_id': 1553073648793276421, 'text': 'Hand Painted Oil Painting On Canvas,Mountains ,', 'score': 0},
